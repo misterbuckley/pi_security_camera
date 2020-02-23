99cdfe432c97aafca052c36e8e9ecd28b7722a0d
Michael Buckley <michael.buckley@cordatahealth.com>
2020-02-23 09:58:37 -0500

    stuff

--- pi_security_camera.py
+++ pi_security_camera.py
@@ -1,8 +1,14 @@
 import io
-import random
 import picamera
+import subprocess
 import numpy as np
 from PIL import Image, ImageChops
+
+
+MOTION_DETECTION_INTERVAL = 1  # attempt to detect motion 1x/sec
+RECORD_INTERVAL = 10  # after motion is detected, record at least 10 seconds
+OUTPUT_DIRECTORY = 'images'
+
 
 prior_image = None
 
@@ -19,16 +25,16 @@ def image_entropy(img):
 def detect_motion(camera):
     global prior_image
     stream = io.BytesIO()
+
     camera.capture(stream, format='jpeg', use_video_port=True)
     stream.seek(0)
+
     if prior_image is None:
         prior_image = Image.open(stream)
         return False
     else:
         current_image = Image.open(stream)
 
-        # TODO detect motion
-        #  result = random.randint(0, 10) == 0
         diff = ImageChops.difference(prior_image, current_image)
         entropy = image_entropy(diff)
         print("entropy: " + str(entropy))
@@ -48,6 +54,7 @@ def write_video(stream):
             if not buf:
                 break
             output.write(buf)
+
     stream.seek(0)
     stream.truncate()
 
@@ -55,23 +62,25 @@ def write_video(stream):
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     stream = picamera.PiCameraCircularIO(camera, seconds=10)
+
     camera.start_recording(stream, format='h264')
+
     try:
         while True:
-            camera.wait_recording(1)
+            camera.wait_recording(MOTION_DETECTION_INTERVAL)
+
             if detect_motion(camera):
-                print('Motion detected!')
-                # As soon as we detect motion, split the recording to
-                # record the frames "after" motion
                 camera.split_recording('after.h264')
-                # Write the 10 seconds "before" motion to disk as well
+
                 write_video(stream)
-                # Wait until motion is no longer detected, then split
-                # recording back to the in-memory circular buffer
+
                 while detect_motion(camera):
-                    camera.wait_recording(1)
+                    camera.wait_recording(RECORD_INTERVAL)
+
+                #  subprocess.call("ffmpeg -framerate 24 -i input.h264 -c copy output.mp4")
                 print('Motion stopped!')
                 camera.split_recording(stream)
+
     finally:
         camera.stop_recording()
 
