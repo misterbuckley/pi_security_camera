718c8ae4a727ba38ca9b0173e5b415b9263a1c1d
Michael Buckley <michael.buckley@cordatahealth.com>
2020-02-23 14:35:58 +0000

    initial commit

--- pi_security_camera.py
+++ pi_security_camera.py
@@ -1,1 +1,73 @@
-#  https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/4
+import io
+import random
+import picamera
+import numpy as np
+from PIL import Image, ImageChops
+
+prior_image = None
+
+def image_entropy(img):
+    w,h = img.size
+    a = np.array(img.convert('RGB')).reshape((w*h,3))
+    h,e = np.histogramdd(a, bins=(16,)*3, range=((0,256),)*3)
+    prob = h/np.sum(h) # normalize
+    prob = prob[prob>0] # remove zeros
+    return -np.sum(prob*np.log2(prob))
+
+def detect_motion(camera):
+    global prior_image
+    stream = io.BytesIO()
+    camera.capture(stream, format='jpeg', use_video_port=True)
+    stream.seek(0)
+    if prior_image is None:
+        prior_image = Image.open(stream)
+        return False
+    else:
+        current_image = Image.open(stream)
+
+        # TODO detect motion
+        #  result = random.randint(0, 10) == 0
+        diff = ImageChops.difference(prior_image, current_image)
+        entropy = image_entropy(diff)
+        print("entropy: " + str(entropy))
+
+        prior_image = current_image
+        return entropy >= 2
+
+def write_video(stream):
+    with io.open('before.h264', 'wb') as output:
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+        while True:
+            buf = stream.read1()
+            if not buf:
+                break
+            output.write(buf)
+    stream.seek(0)
+    stream.truncate()
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    stream = picamera.PiCameraCircularIO(camera, seconds=10)
+    camera.start_recording(stream, format='h264')
+    try:
+        while True:
+            camera.wait_recording(1)
+            if detect_motion(camera):
+                print('Motion detected!')
+                # As soon as we detect motion, split the recording to
+                # record the frames "after" motion
+                camera.split_recording('after.h264')
+                # Write the 10 seconds "before" motion to disk as well
+                write_video(stream)
+                # Wait until motion is no longer detected, then split
+                # recording back to the in-memory circular buffer
+                while detect_motion(camera):
+                    camera.wait_recording(1)
+                print('Motion stopped!')
+                camera.split_recording(stream)
+    finally:
+        camera.stop_recording()
+
